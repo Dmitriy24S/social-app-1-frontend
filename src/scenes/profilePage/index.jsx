fb4f432482feefbf21cd9dd41a95ec6c1b6e2306
Dmitriy24S <devilins24@gmail.com>
2023-03-11 21:00:15 +0200

    style: update profilePage mobile spacing
    * update mobile max-width
    * set margin to center & change padding (2rem 6% to 2rem 1rem)

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -47,7 +47,11 @@ const ProfilePage = () => {
       <Navbar />
       <Box
         width='100%'
-        padding='2rem 6%'
+        // width={isNonMobileScreen ? '100%' : '60%'}
+        maxWidth={isNonMobileScreen ? undefined : '550px'}
+        margin='0 auto'
+        // padding='2rem 6%'
+        padding='2rem 1rem'
         display={isNonMobileScreen ? 'flex' : 'block'}
         gap='0.5rem'
         justifyContent='space-between'
